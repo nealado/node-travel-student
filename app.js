b62a089b42749e5c1d1cac6b1fd3300ca96fc3cc
Victoria <vic.n.d1@gmail.com>
2015-06-21 22:06:04 +0000

    Added travel blog and ice cream template

--- app.js
+++ app.js
@@ -10,5 +10,17 @@ appHelper.runApp(function(app, db) {
 		resp.write("<html><body><h1>Welcome to "+myName+"'s first web app!</body></html>");
 		resp.end();
 	});
-
+  app.get("/icecream/:flavor", function(req, res) {
+     res.render("icecream", {
+         flavor: req.params.flavor
+     });
+  });
+  
+  app.get("/travel", function(req, res) {
+    db.findArray({}, function (results) {
+      res.render("travel", {
+        locations: results
+      })
+    })
+  });
 })
